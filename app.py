c47edc343b3c978fd3792492d4eb7f55e35f4d5c
heoyeonwha <2321048@slp.education>
2026-04-29 18:45:27 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="의사소통기능 분류 도구", page_icon="💬")
+
+st.title("💬 의사소통기능 분류 도구")
+st.write("아동의 발화를 입력하면 의사소통기능을 간단히 분류해주는 웹앱입니다.")
